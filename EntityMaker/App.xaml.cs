8611e741379e65b27296183aa21e77d2f867d7bf
Shunji Kikuchi <sc57.cbr1000rr.04@gmail.com>
2022-02-09 21:56:25 +0900

    ボタン有効/無効をスタイルを設定

--- EntityMaker/App.xaml.cs
+++ EntityMaker/App.xaml.cs
@@ -1,10 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using EntityMaker;
+using System;
+using System.Diagnostics;
 using System.Windows;
+using System.Windows.Automation;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Controls.Ribbon;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Media.TextFormatting;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Shell;
 
 namespace EntityMaker
 {
@@ -13,5 +28,30 @@ namespace EntityMaker
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// InitializeComponent
+        /// </summary>
+        [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
+        public void InitializeComponent()
+        {
+#line 5 "..\..\..\App.xaml"
+            this.StartupUri = new System.Uri("MainWindow.xaml", System.UriKind.Relative);
+#line default
+#line hidden
+        }
+
+        /// <summary>
+        /// Application Entry Point.
+        /// </summary>
+        [System.STAThreadAttribute()]
+        [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
+        public static void Main()
+        {
+            EntityMaker.App app = new EntityMaker.App();
+            app.InitializeComponent();
+            app.Run();
+        }
     }
 }
